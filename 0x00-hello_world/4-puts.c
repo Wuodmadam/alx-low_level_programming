3fd14522190478288f374f19914fb4ad1d6085a5
Wuodmadam <jerrypatrick999@gmail,com>
2023-02-16 09:04:33 -0800

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/** main starts here
+ *opening bracket for body
+ *prints the text
+ *return 0
+ *closing body
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
