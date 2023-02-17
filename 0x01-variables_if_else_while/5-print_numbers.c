ae857f1fe32c29d35b1c7629e6b21c302e159093
Wuodmadam <jerrypatrick999@gmail,com>
2023-02-17 06:29:22 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Prints the numbers of base 10 which are less than 10
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
+
