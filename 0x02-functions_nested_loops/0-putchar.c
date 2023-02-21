ed904a9c5fb791ea6b9c892581b0bd66502ea1fc
Wuodmadam <jerrypatrick999@gmail,com>
2023-02-21 05:39:11 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - the main programm starts here
+ *
+ * Return: should be zero on a succesfull completion
+ */
+int main(void)
+{
+	char *q = "_putchar";
+	
+	while (*q)
+	{
+	_putchar(*q);
+	q++;
+	}
+	_putchar('\n');
+	return (0);
+}
+
