806ac34806a4720bf70698d55b6ec38a2d77d5d9
Wuodmadam <jerrypatrick999@gmail,com>
2023-03-08 05:35:39 -0800

    prime

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,4 +8,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int saidia(int c, int i);
+int is_prime_number(int n);
+int saidia_tena(int n, int d);
+int _sqrt(int x, int i);
 #endif /*MAIN_H*/
