3b8d34408fb0b11723fb04b4d679569f9a2e6427
Wuodmadam <jerrypatrick999@gmail,com>
2023-03-08 05:25:03 -0800

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,17 +9,17 @@
 
 int _sqrt_recursion(int n)
 {
-	return (halp(n, 1));
+	return (saidia(n, 1));
 }
 
 /**
- * halp - helper function to solve _sqrt_recursion
+ * saidia - helper function to solve _sqrt_recursion
  * @c: number to determine if square root
  * @i: incrementer to compare against `c`
  * Return: square root if natural square root, or -1 if none found
  */
 
-int halp(int c, int i)
+int saidia(int c, int i)
 {
 	int square;
 
@@ -27,7 +27,7 @@ int halp(int c, int i)
 	if (square == c)
 		return (i);
 	else if (square < c)
-		return (halp(c, i + 1));
+		return (saidia(c, i + 1));
 	else
 		return (-1);
 }
