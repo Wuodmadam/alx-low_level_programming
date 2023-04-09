98d94c386ddea60f94554d1583c90e7657c07fbb
Wuodmadam <jerrypatrick999@gmail.com>
2023-04-09 12:51:29 -0700

    flip bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,13 +9,13 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int i, nflips = 0;
+	unsigned int i, numberflips = 0;
 	unsigned long int j = sizeof(unsigned long int) * 8;
 
 	for (i = 0; i < j; i++)
 	{
 		if ((m & 1) != (n & 1))
-			nflips += 1;
+			numberflips += 1;
 		n = n >> 1;
 		m = m >> 1;
 	}
